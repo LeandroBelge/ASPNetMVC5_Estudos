b11df360e9394f24015eb08fd86bb8cad0b641bf
LeandroBelge <leandrobelge@gmail.com>
2017-07-26 14:46:25 -0300

    Aula 3 (Criando controllers e views)

---
+++ CaelumEstoque/CaelumEstoque/Controllers/CategoriaController.cs
@@ -0,0 +1,22 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        //
+        // GET: /Categoria/
+        public ActionResult Index()
+        {
+            var categoriaDAO = new CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = categoriaDAO.Lista();
+            return View(categorias);
+        }
+	}
+}
