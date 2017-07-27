c8a97c339f0e687a411dbffdceb4f24561a29a73
LeandroBelge <leandrobelge@gmail.com>
2017-07-27 09:40:15 -0300

    Aula 5 (Validando um produto com Model Validation)

--- CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
+++ CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
@@ -16,23 +16,46 @@ namespace CaelumEstoque.Controllers
         {
             var produtosDAO = new ProdutosDAO();
             IList<Produto> produtos = produtosDAO.Lista();
-            //ViewBag.Produtos = produtos;
-            return View(produtos);
+            ViewBag.Produtos = produtos;
+            return View();
         }
 
         public ActionResult Form()
         {
             var categoriasDAO = new CategoriasDAO();
             IList<CategoriaDoProduto> categorias = categoriasDAO.Lista();
-            return View(categorias);
+            ViewBag.Categorias = categorias;
+            ViewBag.Produto = new Produto();
+            return View();
         }
 
         [HttpPost]
         public ActionResult Adiciona(Produto produto)
         {
-            var Dao = new ProdutosDAO();
-            Dao.Adiciona(produto);
-            return RedirectToAction("Index");
+            this.Validar(produto);
+
+            if (ModelState.IsValid)
+            {
+                var Dao = new ProdutosDAO();
+                Dao.Adiciona(produto);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                var categoriasDAO = new CategoriasDAO();
+                IList<CategoriaDoProduto> categorias = categoriasDAO.Lista();
+                ViewBag.Categorias = categorias;
+                ViewBag.Produto = produto;
+                return View("Form");
+            }
+        }
+
+        private void Validar(Produto produto)
+        {
+            if (produto.CategoriaId == 1 && produto.Preco < 100)
+            {
+                ModelState.AddModelError("produto.Invalido", "O preço do produto não pode ser menor que R$ 100,00");
+            }
         }
 	}
 }
