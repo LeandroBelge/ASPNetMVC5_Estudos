dd9fbb0cb4f35dc9abb32b6a6ee504b0fb4edcbb
LeandroBelge <leandrobelge@gmail.com>
2017-07-28 14:02:38 -0300

    Aula 10 (Sessões para autenticar usuários)

---
+++ CaelumEstoque/CaelumEstoque/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Autentica(string login, string senha)
+        {
+            var DAO = new UsuariosDAO();
+            Usuario usuario = DAO.Busca(login, senha);
+            if (usuario != null)
+            {
+                Session["usuarioLogado"] = usuario;
+                return RedirectToAction("Index", "Produto");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+	}
+}
